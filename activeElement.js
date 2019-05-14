1d9462c413b018c31376dc099fb333adcb9af799
Xiaoxia Zhou <zhouxiaoxia@caicloud.io>
2019-05-14 10:01:17 +0800

    Create activeElement.js

---
+++ activeElement.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    function onMouseUp(e) {
+        var outputElement = document.getElementById('output-element');
+        var outputText = document.getElementById('output-text');
+        var selectedTextArea = document.activeElement;
+        var selection = selectedTextArea.value.substring(
+        selectedTextArea.selectionStart, selectedTextArea.selectionEnd);
+        outputElement.innerHTML = selectedTextArea.id;
+        outputText.innerHTML = selection;
+    }
+
+    document.getElementById("ta-example-one").addEventListener("mouseup", onMouseUp, false);
+    document.getElementById("ta-example-two").addEventListener("mouseup", onMouseUp, false);
+});
